d1f4c2dcce6613a9756c14d31aebf7d10bbbc607
Athira soman <athira.soman@fingent.com>
2020-04-07 20:15:46 +0530

    Database seeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -12,8 +12,10 @@ class ProductSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
-            'name' => "Apple",
-            'detail' => "An apple is a sweet, edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.",
-        ]);
+                'name' => "Apple",
+                'detail' => "An apple is a sweet, edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor, Malus sieversii, is still found today.",
+           
+                ]
+        );
     }
 }
