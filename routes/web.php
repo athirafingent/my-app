8dfd0fada1ef7fbb242440e3d389f25019a0f501
Athira soman <athira.soman@fingent.com>
2020-04-13 20:48:54 +0530

    Contract

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// use Illuminate\Support\Facades\Redis; 
+
+Route::get('/', function () { 
+    print_r(app()->make('redis'));
+    //  $visits = Redis::incr('visits'); 
+    //  return $visits; 
 });
